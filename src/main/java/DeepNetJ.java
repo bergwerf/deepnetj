b0b938c9e3344f62a37b3ccb2ee6e923e0a22898
Herman Bergwerf <hermanbergwerf@gmail.com>
2018-03-28 12:08:51 +0200

    Implement Keras model import

--- src/main/java/DeepNetJ.java
+++ src/main/java/DeepNetJ.java
@@ -5,15 +5,25 @@
  */
 
 import java.io.File;
+import java.util.Arrays;
 
 import net.imagej.ImageJ;
 
+import org.scijava.log.LogService;
 import org.scijava.command.Command;
 import org.scijava.plugin.Plugin;
 import org.scijava.plugin.Parameter;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+
 @Plugin(type=Command.class, headless=true, menuPath="DeepNetJ>Predict")
 public class DeepNetJ implements Command {
+  // Assigned automatically by ImageJ.
+	@Parameter
+  private LogService log;
+
 	@Parameter(label="Keras model and weights (.h5)")
   private File kerasInput;
 
@@ -24,16 +34,29 @@ public class DeepNetJ implements Command {
   })
   private String patchingMethod = "Mirror edges";
 
-  @Parameter(label="Patch input overlap")
+  /*@Parameter(label="Patch input overlap")
   private String patchOverlapIn = "0, 0";
 
   @Parameter(label="Patch output overlap")
-  private String patchOverlapOut = "0, 0";
+  private String patchOverlapOut = "0, 0";*/
 
   // This method is executed when the user pressed OK. This means model
   // parameters are selected, and predicting can begin.
 	@Override
 	public void run() {
-		// Import Keras model and weights into DL4J.
+    try {
+      // Import Keras model and weights into DL4J.
+      String path = kerasInput.getAbsolutePath();
+      ComputationGraph graph = KerasModelImport.importKerasModelAndWeights(path);
+
+      // Log some information about the model to the console.
+      INDArray in = graph.input();
+      log.info("Successfully imported Keras model.");
+      log.info(String.format("Input shape: %s", Arrays.toString(in.shape())));
+      log.info(String.format("Batch size: %s", graph.batchSize()));
+      log.info(String.format("Model summary: %s", graph.summary()));
+    } catch (Exception e) {
+      log.info(String.format("Prediction failed: %s", e.getMessage()));
+    }
 	}
 }
